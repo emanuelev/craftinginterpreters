546c18ca89a4516c8d4b88d052a933f34245ad29
Emanuele Vespa <emanuele.vespa@gmail.com>
2024-10-10 11:12:14 +0200

    Add lox interpreter main

---
+++ python/lox.py
@@ -0,0 +1,45 @@
+"""
+Python implementation of a jlox interpreter
+"""
+import argparse
+import os
+
+
+
+def parse_args():
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "filepath", type=str, help="Path to the jlox script to be interpreted"
+    )
+
+    return parser.parse_args()
+
+
+def run(script: str):
+    """Runs the input lox script
+
+    Args:
+        script: input lox script to be run.
+    """
+
+def run_prompt():
+    """Runs interactive ccommand line prompt"""
+
+def main():
+    """Entry point for the lox interpreter"""
+    args = parse_args()
+    if args.filepath:
+        if os.path.isfile(args.filepath):
+            with open(args.filepath, 'r', encoding="utf-8") as file:
+                script = file.readlines()
+                run(script)
+        else:
+            raise ValueError(f'Provided file {args.filepath} is not a file.')
+    else:
+        run_prompt()
+
+if __name__ == "__main__":
+    main()
